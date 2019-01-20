7ce75717a3b6c1550ab6b15718d2f6b380317dbf
Mark Belaev <mark080897@gmail.com>
2019-01-20 23:22:26 +0200

    10PRINT

---
+++ P5Js/10PRINT/sketch.js
@@ -0,0 +1,31 @@
+let x = 0;
+let y = 0;
+let spacing = 20;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(75);
+    strokeWeight(3);
+    stroke(255);
+}
+
+function draw() {
+    species();
+    x = x + spacing;
+    if (x > width) {
+        x = 0;
+        y = y + spacing;
+    }
+}
+
+function species() {
+    let rnd = int(random(2));
+    switch (rnd) {
+        case 0:
+            line(x, y, x + spacing, y + spacing);
+            break;
+        case 1:
+            line(x, y + spacing, x + spacing, y);
+            break;
+    }
+}
