8686fa93a336e9b5a7035c41d3f3f90325974fd5
Mark Belaev <mark080897@gmail.com>
2019-01-22 20:35:21 +0200

    Fixed fractal tree bugs =)

--- P5Js/FractalTree/branch.js
+++ P5Js/FractalTree/branch.js
@@ -1,52 +1,64 @@
 class Branch {
-    constructor(rootStart, rootEnd, generation) {
+    constructor(rootStart, rootEnd, branches, generation) {
         this.begin = new createVector(rootStart.x, rootStart.y);
         this.end = new createVector(rootEnd.x, rootEnd.y);
         this.finished = false;
-        this.rendered = false;
+        this.branches = branches;
         this.generation = generation;
         this.pathHistoryL = [];
         this.pathHistoryR = [];
         this.dirL;
         this.dirR;
-        this.generation++;
-        //console.log(this.generation);
-        // if (this.generation <= 1) {
-        //     this.nextGen();
-        // }
+
+        if (this.generation <= maxGen) {
+            this.generation++;
+            this.nextGen();
+        }
     }
 
     show() {
-        //this.finished = true;
-        if (this.generation == maxGen + 1) {
+        if (this.generation == maxGen + 1)
             stroke(231, 76, 60);
-        }
         else
             stroke(150, 210, 90);
+
         line(this.begin.x, this.begin.y, this.end.x, this.end.y);
-        this.rendered = true;
+        this.finished = true;
     }
 
-    l() {
-        this.dirL = p5.Vector.sub(this.end, this.begin);
-        this.dirL.mult(data[specie].x);
-        this.dirL.rotate(PI / -9);
-        let newEndL = p5.Vector.add(this.end, this.dirL);
-        let lBranch;
-        lBranch = new Branch(this.end, newEndL, this.generation);
-        return lBranch;
-    }
+    nextGen() {
+        if (this.generation <= maxGen) {
+            this.dirL = p5.Vector.sub(this.end, this.begin);
+            this.dirR = p5.Vector.sub(this.end, this.begin);
 
-    r() {
-        this.dirR = p5.Vector.sub(this.end, this.begin);
-        this.dirR.mult(data[specie].y);
-        this.dirR.rotate(PI / 9);
-        let newEndR = p5.Vector.add(this.end, this.dirR);
-        let rBranch;
-        rBranch = new Branch(this.end, newEndR, this.generation);
-        return rBranch;
-    }
+            this.dirL.mult(specieData[specie].x);
+            this.dirR.mult(specieData[specie].y);
 
+            this.dirL.rotate(angleData[angle].x);
+            this.dirR.rotate(angleData[angle].y);
+
+            let newEndL = p5.Vector.add(this.end, this.dirL);
+            let newEndR = p5.Vector.add(this.end, this.dirR);
+            let lBranch; let rBranch;
+            // if (this.generation > 8) {
+            //     if (random(1) > 0.5) {
+            //         lBranch = new Branch(this.end, newEndL, this.branches, this.generation);
+            //         this.branches.push(lBranch);
+            //     }
+            //     else {
+            //         rBranch = new Branch(this.end, newEndR, this.branches, this.generation);
+            //         this.branches.push(rBranch);
+            //     }
+            // } else {
+            rBranch = new Branch(this.end, newEndR, this.branches, this.generation);
+            lBranch = new Branch(this.end, newEndL, this.branches, this.generation);
+            this.branches.push(rBranch);
+            this.branches.push(lBranch);
+            //}
+            //this.divideBranch();
+            //this.show();
+        }
+    }
 
     // divideBranch() {
     //     let accumL = this.end.copy();
@@ -63,7 +75,6 @@ class Branch {
     //         accumL = p5.Vector.add(accumL, stepL);
     //         accumR = p5.Vector.add(accumR, stepR);
     //     }
-
     //     //this._debugDivision();
     // }
 
@@ -76,50 +87,3 @@ class Branch {
     //     strokeWeight(3);
     // }
 }
-
-
-// const sleep = (milliseconds) => {
-//     return new Promise(resolve => setTimeout(resolve, milliseconds))
-// }
-
-// POSSIBLE FIX: DONT CREATE NEW BRANCHES UNTILL OLD ONE IS FULLY RENDERED 0_0
-
-// let x = 4;
-// if (x == 1) {
-//     for (let i = this.pathHistoryR.length - 2; i >= 0; i--) {
-//         line(this.pathHistoryR[i + 1].x, this.pathHistoryR[i + 1].y, this.pathHistoryR[i].x, this.pathHistoryR[i].y);
-//         line(this.pathHistoryL[i + 1].x, this.pathHistoryL[i + 1].y, this.pathHistoryL[i].x, this.pathHistoryL[i].y);
-//     }
-// }
-// else if (x == 2) {
-//     for (let i = this.pathHistoryR.length - 2; i >= 0; i--) {
-//         line(this.pathHistoryR[i + 1].x, this.pathHistoryR[i + 1].y, this.pathHistoryR[i].x, this.pathHistoryR[i].y);
-//         line(this.pathHistoryL[i + 1].x, this.pathHistoryL[i + 1].y, this.pathHistoryL[i].x, this.pathHistoryL[i].y);
-//         sleep(10).then(() => { })
-//     }
-// }
-// else if (x == 3) {
-//     (function myLoop(i, pathL, pathR) {
-//         setTimeout(function () {
-//             line(pathL[i + 1].x, pathL[i + 1].y, pathL[i].x, pathL[i].y);
-//             line(pathR[i + 1].x, pathR[i + 1].y, pathR[i].x, pathR[i].y);
-//             i--;
-//             if (i >= 0) myLoop(i, pathL, pathR);      //  decrement i and call myLoop again if i > 0
-//         }, 20)
-//     })(this.pathHistoryR.length - 2, this.pathHistoryL, this.pathHistoryR);
-// } else {
-
-// }
-
-//let qwe = map(mouseX, 0, width, TWO_PI, TWO_PI + PI);
-
-            // if (this.generation > 8) {
-            //     if (random(1) > 0.5) {
-            //         lBranch = new Branch(this.end, newEndL, this.branches, this.generation);
-            //         this.branches.push(lBranch);
-            //     }
-            //     else {
-            //         rBranch = new Branch(this.end, newEndR, this.branches, this.generation);
-            //         this.branches.push(rBranch);
-            //     }
-            // } else {
--- P5Js/FractalTree/sketch.js
+++ P5Js/FractalTree/sketch.js
@@ -1,92 +1,68 @@
 let tree = [];
-let counter = 0;
 let frameR = 60;
-let data = [];
+
+let specieData = [];
+let angleData = [];
 let specie = 1;
+let angle = 3;
+
 let divider = 10;
 let trigger = false;
-let maxGen = 0;
-let delay = 5;
-let notBorn = true;
-let ready = false;
-let root;
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(75);
-	strokeWeight(3);
-	stroke(150, 210, 90);
-	frameRate(frameR);
-	createData();
+let maxGen = 8;
+let delay = 10;
 
-	let rootStart = new createVector(width / 2, height);
-	let rootEnd = new createVector(width / 2, height - 200); //-500 (Pi/2)
-	root = new Branch(rootStart, rootEnd, 0);
-	tree[0] = root;
-	//line(rootStart.x, rootStart.y, rootEnd.x, rootEnd.y);
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(75);
+    strokeWeight(3);
+    stroke(150, 210, 90);
+    frameRate(frameR);
+    createSpecieData();
+    createAngleData();
+    let rootStart = new createVector(width / 2, height);
+    let rootEnd = new createVector(width / 2, height - 200); //-500 (Pi/2)
+    new Branch(rootStart, rootEnd, tree, 0); //START
+    line(rootStart.x, rootStart.y, rootEnd.x, rootEnd.y);
 }
 
 function draw() {
-	if (notBorn) {
-		if (maxGen <= 15) {
-			for (var i = tree.length - 1; i >= 0; i--) {
-				if (!tree[i].finished) {
-					tree.push(tree[i].r());
-					tree.push(tree[i].l());
-				}
-				tree[i].finished = true;
-				maxGen++;
-			}
-			tree.sort(compare);
-		}
-		else
-			notBorn = false
-	}
-
-	if (trigger) {
-		(function myLoop(i) {
-			setTimeout(function () {
-				if (!tree[i].rendered) {
-					//background(75);
-					tree[i].show();
-					//console.log(i);
-				}
-				i++;
-				if (i <= tree.length - 1) myLoop(i);
-			}, delay)
-		})(0);
-
-		// for (var i = 0; i < tree.length; i++) {
-		// 	tree[i].show();
-		// 	//tree[i].jitter();
-		// }
-	}
+    if (trigger) {
+        iterate();
+    }
 }
-
-function compare(a, b) {
-	if (a.begin.y > b.begin.y)
-		return -1;
-	if (a.begin.y < b.begin.y)
-		return 1;
-	return 0;
-}
-
-// function compare2(a, b) {
-// 	if ()
-// 		return -1;
-// 	if (a.begin.y < b.begin.y)
-// 		return 1;
-// 	return 0;
-// }
-
 
 function mousePressed() {
-	trigger = !trigger;
-	notBorn = false;
+    trigger = !trigger;
 }
 
-function createData() {
-	data[0] = createVector(0.5, 0.5);
-	data[1] = createVector(0.75, 0.75);
-	data[2] = createVector(0.25, 0.75);
-	data[3] = createVector(0.75, 0.75);
+function createSpecieData() {
+    specieData[0] = createVector(0.5, 0.5);
+    specieData[1] = createVector(0.75, 0.75);
+    specieData[2] = createVector(0.25, 0.75);
+    specieData[3] = createVector(0.75, 0.75);
+    specieData[4] = createVector(1, 1);
+}
+
+function createAngleData() {
+    angleData[0] = createVector(-PI / 2, PI / 2);
+    angleData[1] = createVector(-PI / 4, PI / 4);
+    angleData[2] = createVector(-PI / 6, PI / 6);
+    angleData[3] = createVector(-PI / 8, PI / 8);
+}
+
+function w8(time) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, time);
+    });
+}
+
+async function iterate() {
+    for (let i = tree.length - 1; i >= 0; i--) {
+        if (!tree[i].finished) {
+            tree[i].show();
+        }
+        await w8(delay);
+    }
 }
