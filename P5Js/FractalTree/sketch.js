38f5b4a51ed157f3f3c0f54ed19e83b40f1064b3
Mark Belaev <mark080897@gmail.com>
2019-01-22 03:30:07 +0200

    hardish

--- P5Js/FractalTree/sketch.js
+++ P5Js/FractalTree/sketch.js
@@ -2,12 +2,14 @@ let tree = [];
 let counter = 0;
 let frameR = 60;
 let data = [];
-let specie = 1;
+let specie = 2;
 let divider = 10;
 let trigger = false;
-let maxGen = 14;
+let maxGen = 0;
 let delay = 10;
-
+let notBorn = true;
+let ready = false;
+let root;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(75);
@@ -15,35 +17,58 @@ function setup() {
 	stroke(150, 210, 90);
 	frameRate(frameR);
 	createData();
+
 	let rootStart = new createVector(width / 2, height);
 	let rootEnd = new createVector(width / 2, height - 200); //-500 (Pi/2)
-	let root = new Branch(rootStart, rootEnd, tree, 0);
-	line(rootStart.x, rootStart.y, rootEnd.x, rootEnd.y);
+	root = new Branch(rootStart, rootEnd, 0);
 	tree[0] = root;
-	tree[0].nextGen();
+	//line(rootStart.x, rootStart.y, rootEnd.x, rootEnd.y);
 }
 
 function draw() {
+	if (notBorn) {
+		if (maxGen <= 5000) {
+			for (var i = tree.length - 1; i >= 0; i--) {
+				if (!tree[i].finished) {
+					tree.push(tree[i].r());
+					tree.push(tree[i].l());
+				}
+				tree[i].finished = true;
+				maxGen++;
+			}
+		}
+		else
+			notBorn = false
+	}
+
 	if (trigger) {
 		(function myLoop(i) {
 			setTimeout(function () {
-				if (!tree[i].finished) {
+				if (!tree[i].rendered) {
 					//background(75);
 					tree[i].show();
+					//console.log(i);
 				}
-				i--;
-				if (i >= 0) myLoop(i);
+				i++;
+				if (i <= tree.length - 1) myLoop(i);
 			}, delay)
-		})(tree.length - 1);
+		})(0);
+
+		// for (var i = 0; i < tree.length; i++) {
+		// 	tree[i].show();
+		// 	//tree[i].jitter();
+		// }
 	}
 }
 
 function mousePressed() {
 	trigger = !trigger;
+	notBorn = false;
 }
 
 function createData() {
 	data[0] = createVector(0.5, 0.5);
 	data[1] = createVector(0.75, 0.75);
 	data[2] = createVector(0.25, 0.75);
+	data[3] = createVector(0.75, 0.75);
 }
